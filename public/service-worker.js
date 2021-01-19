0649f2ed6da1a15b2c98b007442aadcb0dc70803
Jehyun <congmul@hotmail.com>
2021-01-18 22:14:12 -0800

    Add all static files in Cache

---
+++ public/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+
+const iconFiles = iconSizes.map(
+    (size) => `icons/icon-${size}x${size}.png`
+  );
+
+
+const staticFilesToPreCache = [
+    "/",
+    "index.html",
+    "/index.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+].concat(iconFiles);
+
+
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(staticFilesToPreCache);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+
+//   // activate
+//   self.addEventListener("activate", function(evt) {
+//     evt.waitUntil(
+//       caches.keys().then(keyList => {
+//         return Promise.all(
+//           keyList.map(key => {
+//             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//               console.log("Removing old cache data", key);
+//               return caches.delete(key);
+//             }
+//           })
+//         );
+//       })
+//     );
+  
+//     self.clients.claim();
+//   });
